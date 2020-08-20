24c9bf3d7ed281da5fa28e66e02b91da32c130ab
flashpoint1986@mail.ru <flashpoint1986@mail.ru>
2020-08-20 19:24:12 +0300

    Удаление записей и изображений с главной страницы

--- ASP_NewsModule/Controllers/AdminController.cs
+++ ASP_NewsModule/Controllers/AdminController.cs
@@ -29,7 +29,7 @@ namespace ASP_NewsModule.Controllers
             return View();
         }
 
-        #region Создание новости [GET]
+        #region Создать новость [GET]
         [HttpGet]
         public IActionResult AddNews()
         {
@@ -37,7 +37,7 @@ namespace ASP_NewsModule.Controllers
         }
         #endregion
 
-        #region Создание новости [POST]
+        #region Создать новость [POST]
         [HttpPost]
         public async Task<IActionResult> AddNews(AddNewsViewModel model, IFormFileCollection uploads)
         {
@@ -71,12 +71,13 @@ namespace ASP_NewsModule.Controllers
                     // Если размер входного файла больше 0, заходим в тело условия
                     if (uploadedImage.Length > 0)
                     {
-                        // Присваиваем загружаемому файлу уникальное имя на основе Guid
+                        // Присваиваем загружаемому файлу уникальное имя на основе Guid и приводим имя к нижней нотации
                         string imageName = Guid.NewGuid() + "_" + uploadedImage.FileName.ToLower();
                         // Пути сохранения файла
                         string pathNormal = "/files/images/normal/" + imageName; // изображение исходного размера
                         string pathScaled = "/files/images/scaled/" + imageName; // уменьшенное изображение
-                        // В try делаем уменьшенную копию изображения. Так же оператор try/catch выступает дополнительной проверкой типа загружаемого файла
+
+                        // В операторе try/catch делаем уменьшенную копию изображения. Так же этот оператор выступает дополнительной проверкой на тип загружаемого файла
                         try
                         {
                             using (var stream = uploadedImage.OpenReadStream())
@@ -89,11 +90,12 @@ namespace ASP_NewsModule.Controllers
                                 }
                             }
                         }
-                        // Если вдруг что-то пошло не так (например, на вход подало не картинку), то выводим сообщение об ошибке. Костыль, но пока так.
+                        // Если вдруг что-то пошло не так (например, на вход подало не картинку), то выводим сообщение об ошибке
                         catch
                         {
                             // Создаем сообщение об ошибке для вывода пользователю
                             ModelState.AddModelError("NewsImage", $"Файл {uploadedImage.FileName} имеет неверный формат.");
+
                             // Удаляем только что созданные файлы (если ошибка возникла не на первом файле)
                             foreach (var image in newsImages)
                             {
@@ -110,6 +112,7 @@ namespace ASP_NewsModule.Controllers
                                     imageScaled.Delete();
                                 }
                             }
+
                             // Возвращаем модель с сообщением об ошибке в представление
                             return View(model);
                         }
@@ -119,6 +122,7 @@ namespace ASP_NewsModule.Controllers
                         {
                             await uploadedImage.CopyToAsync(file);
                         }
+
                         // Создаем объект класса NewsImage со всеми параметрами
                         NewsImage newsImage = new NewsImage()
                         {
@@ -128,17 +132,65 @@ namespace ASP_NewsModule.Controllers
                             ImagePathScaled = pathScaled,
                             NewsId = news.Id
                         };
+                        // Добавляем объект newsImage в список newsImages
                         newsImages.Add(newsImage);
                     }
                 }
 
-                // Сохраняем всё в БД
+                // Если в процессе выполнения не возникло ошибок, сохраняем всё в БД
                 await newsDB.NewsImages.AddRangeAsync(newsImages);
                 await newsDB.News.AddAsync(news);
                 await newsDB.SaveChangesAsync();
+
+                // Редирект на главную страницу
                 return RedirectToAction("Index", "Home");
             }
+
+            // Возврат модели в представление в случае, если запорится валидация
             return View(model);
+        }
+        #endregion
+
+        #region Удалить новость [POST]
+        public async Task<IActionResult> DeleteNews(Guid newsId)
+        {
+            // Создаем экземпляр новости для удаления. Достаточно просто присвоить Id удаляемой записи
+            News news = new News { Id = newsId };
+
+            // Создаем список для привязанных к удаляемой записи изображений
+            List<NewsImage> newsImages = new List<NewsImage>();
+            // Находим привязанные изображения и кладём их в список
+            foreach (var image in newsDB.NewsImages)
+            {
+                if (image.NewsId == newsId)
+                {
+                    newsImages.Add(image);
+                }
+            }
+
+            // Удаление изображений из папок
+            foreach (var image in newsImages)
+            {
+                // Исходные (полноразмерные) изображения
+                FileInfo imageNormal = new FileInfo(_appEnvironment.WebRootPath + image.ImagePathNormal);
+                if (imageNormal.Exists)
+                {
+                    imageNormal.Delete();
+                }
+                // И их уменьшенные копии
+                FileInfo imageScaled = new FileInfo(_appEnvironment.WebRootPath + image.ImagePathScaled);
+                if (imageScaled.Exists)
+                {
+                    imageScaled.Delete();
+                }
+            }
+
+            // Удаление данных из БД
+            newsDB.NewsImages.RemoveRange(newsImages); // Эта строка не обязательна, т.к. при удалении новости, записи с изображениями теряют связь по Id и трутся сами
+            newsDB.News.Remove(news);
+            await newsDB.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Home");
         }
         #endregion
     }
--- ASP_NewsModule/Controllers/HomeController.cs
+++ ASP_NewsModule/Controllers/HomeController.cs
@@ -20,28 +20,42 @@ namespace ASP_NewsModule.Controllers
             newsDB = newsContext;
         }
 
-        public async Task<IActionResult> Index(int pageNumber = 1)
+        #region Главная страница
+        public async Task<IActionResult> Index(int pageNumber = 1) // Страница по умолчанию = 1
         {
             // Количество записей на страницу
             int pageSize = 10;
 
+            // Формируем список записей для обработки перед выводом на страницу
             IQueryable<News> source = newsDB.News;
+
+            // Рассчитываем, какие именно записи будут выведены на странице
             List<News> news = await source.OrderByDescending(n => n.NewsDate).Skip((pageNumber - 1) * pageSize).Take(pageSize).ToListAsync();
+
+            // Общее количество записей для дальнейшего рассчета количества страниц
             int newsCount = await source.CountAsync();
 
-            Guid[] arr = news.Select(n => n.Id).ToArray();
+            // Создаем массив Id-шников записей для выборки изображений к ним
+            Guid[] newsIdArray = news.Select(n => n.Id).ToArray();
+
+            // Создаем список из изображений, которые будут поданы вместе со списом записей
             List<NewsImage> newsImages = new List<NewsImage>();
+
+            // Перебираем изображения в БД
             foreach (var image in newsDB.NewsImages)
             {
-                foreach (var item in arr)
+                // Перебираем все элементы ранее созданного массива Guid[] newsIdArray
+                foreach (var newsId in newsIdArray)
                 {
-                    if (image.NewsId == item)
+                    // Если данные совпадают, то кладём изображение в список для вывода на странице
+                    if (image.NewsId == newsId)
                     {
                         newsImages.Add(image);
                     }
                 }
             }
 
+            // Создаём модель для вывода на странице и кладём в неё все необходимые данные
             IndexViewModel model = new IndexViewModel()
             {
                 News = news,
@@ -50,7 +64,9 @@ namespace ASP_NewsModule.Controllers
                 TotalPages = (int)Math.Ceiling(newsCount / (double)pageSize)
             };
 
+            // Выводим модель в представление
             return View(model);
         }
+        #endregion
     }
 }
